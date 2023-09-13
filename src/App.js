659239b872d4d6968c7d3778f2edd7c7382900e1
ThaiVanHandSome <danitbadao1234@gmail.com>
2023-09-13 11:44:54 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <h1>PROJECT</h1>;
+}
+
+export default App;
---
+++ src/styles/GlobalStyle/index.js
@@ -0,0 +1,5 @@
+function GlobalStyle({ children }) {
+    return <div className="wrapper">{children}</div>;
+}
+
+export default GlobalStyle;
