88dafaf9f730b9e820cb302844e5afd3057c833b
ThaiVanHandSome <danitbadao1234@gmail.com>
2023-09-15 13:40:44 +0700

    ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './Home.module.scss';
+import { Button } from 'reactstrap';
 
 const cx = classNames.bind(styles);
 
 function Home() {
     return (
         <div className={cx('wrapper')}>
-            <h1>Home page</h1>
+            <div className={cx('area')}></div>
+            <ul className={cx('circles')}>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <div className={cx('container')}>
+                <Button tag="Link" to="/cc" className={cx('btn')} color="warning" size="lg">
+                    CREATE
+                </Button>
+                <Button className={cx('btn')} color="danger" size="lg">
+                    VIEW
+                </Button>
+            </div>
         </div>
     );
 }
